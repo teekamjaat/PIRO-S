29bb0c8656e7a3b1d242e15cbf55df2b52c2c440
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2025-03-18 14:04:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -181,19 +181,15 @@ app.run()
 
 
 
-from flask import Flask  
-import os  
+import os
+from flask import Flask
 
-app = Flask(__name__)  
+app = Flask(__name__)
 
-@app.route("/")  
-def home():  
-    return "OK"  
+@app.route("/")
+def home():
+    return "OK"
 
-if __name__ == "__main__":  
-    port = int(os.environ.get("PORT", 8000))  
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Use Koyeb's assigned port
     app.run(host="0.0.0.0", port=port)
-
-
-
-
