6e10a67695e11dfef453981b86d5853a38ae27aa
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2025-04-16 16:52:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -31,7 +31,7 @@ async def pm_search(client, message):
     if PM_SEARCH:
         await auto_filter(client, message)  
     else:
-        await message.reply_text("⚠️ ʏᴏᴜ ᴄᴀɴ'ᴛ ɢᴇᴛ ᴍᴏᴠɪᴇ ғʀᴏᴍ ʜᴇʀ\n\nsᴇᴀʀᴄʜ 🔍 ᴍᴏᴠɪᴇ ɪɴ ᴏᴜʀ ɢʀᴏᴜᴘ\n\nhttps://t.me/+sBQBAjfyKqM5YWY1\nhttps://t.me/+sBQBAjfyKqM5YWY1")
+        await message.reply_text("➜ ⚠️ ʏᴏᴜ ᴄᴀɴ'ᴛ ɢᴇᴛ ᴍᴏᴠɪᴇ ғʀᴏᴍ ʜᴇʀ\n➜ sᴇᴀʀᴄʜ 🔍 ᴍᴏᴠɪᴇ ɪɴ ᴏᴜʀ ɢʀᴏᴜᴘ\n\nhttps://t.me/+WmC7Vjg5EiszMDc1\nhttps://t.me/+WmC7Vjg5EiszMDc1")
     
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def group_search(client, message):
