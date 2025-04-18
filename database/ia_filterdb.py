1de392c57de5d3b8f3dfcca911bea19305896512
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2025-04-18 17:15:13 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -15,7 +15,7 @@ instance = Instance.from_db(mydb)
 
 @instance.register
 class Media(Document):
-    file_id = fields.StrField(attribute='_id')
+    file_id = fields.StrField(attribute='file_id')
     file_ref = fields.StrField(allow_none=True)
     file_name = fields.StrField(required=True)
     file_size = fields.IntField(required=True)
