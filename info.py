bab45c875aa8eea96eef44e41648a5532d9281fb
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2025-05-01 09:21:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,8 +26,8 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 USERNAME = environ.get('USERNAME', 'https://telegram.me/teekam_jaat')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001786924542'))
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001983152794 -1001756564118 -1002369988753').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://teekam1:teekam@ipopcornbot.uszc1z1.mongodb.net/?retryWrites=true&w=majority&appName=Ipopcornbot")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://Teekam1:teekam@ipopcornbot1.4pteojl.mongodb.net/?retryWrites=true&w=majority&appName=Ipopcornbot1")
 DATABASE_NAME = environ.get('DATABASE_NAME', "tjbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'tjbotdatabase')
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1001786924542'))
