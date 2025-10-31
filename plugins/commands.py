0a9073456404fa7f4fca0568f46a2c8bde6f67a4
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2025-11-01 01:12:12 +0530

    Update join buttons for movie group and channels

--- plugins/commands.py
+++ plugins/commands.py
@@ -134,12 +134,10 @@ async def start(client:Client, message):
     if settings.get('fsub_id', AUTH_CHANNEL) and not await is_subscribed(client, message.from_user.id, channel):
         invite_link = await client.create_chat_invite_link(channel, creates_join_request=True)
         btn = [[
-                InlineKeyboardButton("⛔️ ᴊᴏɪɴ ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ ⛔️", url=invite_link.invite_link)
+                InlineKeyboardButton("⛔️ ᴊᴏɪɴ ʙᴀᴄᴋᴜᴘ ᴄʜᴀɴɴᴇʟ ⛔️", url=invite_link.invite_link)
                 ],[
-                InlineKeyboardButton("⛔️ ᴊᴏɪɴ ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ 1 ⛔️", url='https://t.me/+PrmGkaL_FXVjZDg1')
-              ],[
-                InlineKeyboardButton("⛔️ ᴊᴏɪɴ ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ 2 ⛔️", url='https://t.me/+bdOuyhoo6OFiNmE1')
-        ]]
+                InlineKeyboardButton("⛔️ ᴊᴏɪɴ ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ 1 ⛔️", url='https://t.me/+bdOuyhoo6OFiNmE1')
+              ]]
         if message.command[1] != "subscribe":
             btn.append([InlineKeyboardButton("♻️ ᴛʀʏ ᴀɢᴀɪɴ ♻️", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
         await client.send_message(
