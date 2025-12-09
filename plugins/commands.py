21cc6d359695ead43640ea64f34f2c05e0566c52
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2025-12-09 23:12:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -137,7 +137,9 @@ async def start(client:Client, message):
                 InlineKeyboardButton("⛔️ ᴊᴏɪɴ ʙᴀᴄᴋᴜᴘ ᴄʜᴀɴɴᴇʟ ⛔️", url=invite_link.invite_link)
                 ],[
                 InlineKeyboardButton("⛔️ ᴊᴏɪɴ ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ 1 ⛔️", url='https://t.me/+bdOuyhoo6OFiNmE1')
-              ]]
+              ],[
+                InlineKeyboardButton("⛔️ ᴊᴏɪɴ ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ 2 ⛔️", url='https://t.me/+0hXpTzLijBcxNjc9')
+              ]
         if message.command[1] != "subscribe":
             btn.append([InlineKeyboardButton("♻️ ᴛʀʏ ᴀɢᴀɪɴ ♻️", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
         await client.send_message(
